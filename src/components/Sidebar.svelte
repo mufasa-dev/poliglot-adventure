6623284a23d3224d12b734770bdb5628ed1220dd
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-09-15 20:46:53 -0300

    feat: ajust routes

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,3 +1,12 @@
+<script>
+  let currentPath = '';
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    currentPath = window.location.pathname;
+  });
+</script>
+
 <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 220px; height: 100vh;">
   <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
     <span class="fs-5">Menu</span>
@@ -5,16 +14,16 @@
   <hr />
   <ul class="nav nav-pills flex-column mb-auto">
     <li class="nav-item">
-      <a href="/" class="nav-link active text-white">Início</a>
+        <a href="/" class="nav-link text-white">Início</a>
     </li>
     <li>
-      <a href="/exercicios" class="nav-link text-white">Exercícios</a>
+        <a href="/homework" class="nav-link text-white">Exercícios</a>
     </li>
     <li>
-      <a href="/conversas" class="nav-link text-white">Conversas</a>
+        <a href="/chat" class="nav-link text-white {currentPath === '/chat' ? 'active' : ''}">Conversas</a>
     </li>
     <li>
-      <a href="/perfil" class="nav-link text-white">Perfil</a>
+        <a href="/perfil" class="nav-link text-white">Perfil</a>
     </li>
   </ul>
 </div>
