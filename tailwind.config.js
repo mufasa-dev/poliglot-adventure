788d3cfd36da7757dbb3509ccaacdd2cc0557da2
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-09-15 20:36:43 -0300

    feat: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,svelte,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
