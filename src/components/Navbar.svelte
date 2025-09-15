e036331260ee87713a6f6119047b1a4b7523451f
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-09-15 19:45:37 -0300

    feat: start layout

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+  <a class="navbar-brand" href="/">LinguaQuest</a>
+  <div class="collapse navbar-collapse">
+    <ul class="navbar-nav ms-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="/login">Sair</a>
+      </li>
+    </ul>
+  </div>
+</nav>
---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,20 @@
+<div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 220px; height: 100vh;">
+  <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+    <span class="fs-5">Menu</span>
+  </a>
+  <hr />
+  <ul class="nav nav-pills flex-column mb-auto">
+    <li class="nav-item">
+      <a href="/" class="nav-link active text-white">Início</a>
+    </li>
+    <li>
+      <a href="/exercicios" class="nav-link text-white">Exercícios</a>
+    </li>
+    <li>
+      <a href="/conversas" class="nav-link text-white">Conversas</a>
+    </li>
+    <li>
+      <a href="/perfil" class="nav-link text-white">Perfil</a>
+    </li>
+  </ul>
+</div>
