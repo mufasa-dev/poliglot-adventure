c4dbda2ed56155a44981e420684581914b2c7b50
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-09-18 22:00:51 -0300

    feat: add login

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -2,9 +2,19 @@
   let email = "";
   let password = "";
 
-  function login() {
-    alert(`Login com email: ${email}, senha: ${password}`);
-    // Aqui depois você chama sua API
+  async function login() {
+    const res = await fetch("/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password })
+    });
+
+    const data = await res.json();
+    if (res.ok) {
+      window.location.href = "/main";
+    } else {
+      alert("Erro: " + data.error);
+    }
   }
 </script>
 
@@ -12,11 +22,11 @@
   <div class="h-full p-4 w-1/2 flex flex-col justify-center items-center bg-bg-secondary text-text-primary rounded-md">
     <h2 class="mb-3">Entrar</h2>
     <div class="mb-3">
-      <label class="form-label">Email</label>
-      <input type="email" bind:value={email} class="input-base mb-3" placeholder="Digite seu email" />
+      <label for="login-email" class="form-label">Email</label>
+      <input id="login-email" type="email" bind:value={email} class="input-base mb-3" placeholder="Digite seu email" />
 
-      <label class="form-label">Senha</label>
-      <input type="password" bind:value={password} class="input-base mb-3" placeholder="Digite sua senha" />
+      <label for="login-password" class="form-label">Senha</label>
+      <input id="login-password" type="password" bind:value={password} class="input-base mb-3" placeholder="Digite sua senha" />
 
       <button class="btn-primary w-full" on:click={login}>Login</button>
 
