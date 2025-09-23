5b9e514622ad0c04060a39cf66dd09862de3b655
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-09-23 17:04:13 -0300

    feat: show course flag on sidebar

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,10 +1,10 @@
-<script>
+<script lang="ts">
   import { onMount } from "svelte";
   import { faUser, faComments, faTasks, faDoorOpen, faFlag } from "@fortawesome/free-solid-svg-icons";
   import { FontAwesomeIcon } from "@fortawesome/svelte-fontawesome";
 
   let currentPath = "";
-  let course = {};
+  let course = "";
 
   onMount(async () => {
     currentPath = window.location.pathname;
@@ -38,13 +38,34 @@
 
     window.location.href = "/login";
   }
+
+  function getFlagCourse(name: string) {
+    if (!name) return "/courses/pt-br.png";
+    name = name.toLowerCase();
+    switch (name) {
+      case "en-us":
+      case "inglês":
+        return "/courses/en-us.png";
+      case "es-es":
+      case "espanhol":
+        return "/courses/es-es.png";
+      case "fr-fr":
+      case "francês":
+        return "/courses/fr-fr.png";
+      case "ja-jp":
+      case "japonês":
+        return "/courses/fr-fr.png";
+      default:
+        return "/courses/pt-br.png";
+    }
+  }
 </script>
 
 <!-- Sidebar -->
 <div class="flex flex-col p-4 bg-bg-secondary text-white w-56 h-screen shadow-lg">
   <a href="/select-course" class="block px-3 mb-3 py-2 rounded-md transition-colors 
       {currentPath === '/select-course' ? 'bg-accent text-white' : 'hover:bg-btn-primary'}">
-    <img src="/courses/en-us.png" alt="Logo" class="h-10 mx-auto" />
+    <img src="{getFlagCourse(course)}" alt="Logo" class="h-10 mx-auto" />
   </a>
 
   <ul class="flex flex-col space-y-2 text-black">
