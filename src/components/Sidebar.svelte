cb636998aec3afd197412c9a140361da8aa161c9
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-09-22 22:17:42 -0300

    feat: add fontawesome icons

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,10 +1,10 @@
 <script>
   import { onMount } from "svelte";
+  import { faUser, faComments, faTasks, faDoorOpen, faFlag } from "@fortawesome/free-solid-svg-icons";
+  import { FontAwesomeIcon } from "@fortawesome/svelte-fontawesome";
 
   let currentPath = "";
-  let showCourseModal = false;
-  let selectedCourse = "";
-  let courses = ["Inglês", "Espanhol", "Italiano", "Japonês"];
+  let course = {};
 
   onMount(async () => {
     currentPath = window.location.pathname;
@@ -24,13 +24,15 @@
 
     if (res.ok) {
       const data = await res.json();
-      if (!data.course) {
-        showCourseModal = true;
+      if (!data.course && currentPath !== "/select-course" && currentPath !== "/" && currentPath !== "/profile") {
+        window.location.href = "/select-course";
       }
+      course = data.course;
     } else {
       console.error("Erro ao verificar curso");
     }
   });
+
   function logout() {
     localStorage.removeItem("token");
 
@@ -45,34 +47,36 @@
     <img src="/courses/en-us.png" alt="Logo" class="h-10 mx-auto" />
   </a>
 
-  <ul class="flex flex-col space-y-2">
-    <li>
-      <a href="/" class="block px-3 py-2 rounded-md transition-colors 
-        {currentPath === '/' ? 'bg-accent text-white' : 'hover:bg-btn-primary'}">
-        Início
-      </a>
-    </li>
-    <li>
-      <a href="/homework" class="block px-3 py-2 rounded-md transition-colors 
-        {currentPath === '/homework' ? 'bg-accent text-white' : 'hover:bg-btn-primary'}">
-        Exercícios
-      </a>
-    </li>
-    <li>
-      <a href="/chat" class="block px-3 py-2 rounded-md transition-colors 
-        {currentPath === '/chat' ? 'bg-accent text-white' : 'hover:bg-btn-primary'}">
-        Conversas
-      </a>
-    </li>
+  <ul class="flex flex-col space-y-2 text-black">
+    {#if course != null}
+      <li>
+        <a href="/" class="block px-3 py-2 rounded-md transition-colors 
+          {currentPath === '/' ? 'bg-accent text-white' : 'hover:bg-btn-primary hover:text-white'}">
+          <FontAwesomeIcon icon={faFlag} class="w-4 h-4" /> Início
+        </a>
+      </li>
+      <li>
+        <a href="/homework" class="block px-3 py-2 rounded-md transition-colors 
+          {currentPath === '/homework' ? 'bg-accent text-white' : 'hover:bg-btn-primary hover:text-white'}">
+          <FontAwesomeIcon icon={faTasks} class="w-4 h-4" /> Exercícios
+        </a>
+      </li>
+      <li>
+        <a href="/chat" class="block px-3 py-2 rounded-md transition-colors 
+          {currentPath === '/chat' ? 'bg-accent text-white' : 'hover:bg-btn-primary hover:text-white'}">
+          <FontAwesomeIcon icon={faComments} class="w-4 h-4" /> Conversas
+        </a>
+      </li>
+    {/if}
     <li>
       <a href="/perfil" class="block px-3 py-2 rounded-md transition-colors 
-        {currentPath === '/perfil' ? 'bg-accent text-white' : 'hover:bg-btn-primary'}">
-        Perfil
+        {currentPath === '/perfil' ? 'bg-accent text-white' : 'hover:bg-btn-primary hover:text-white'}">
+        <FontAwesomeIcon icon={faUser} class="w-4 h-4" /> Perfil
       </a>
     </li>
     <li>
-      <button class="block px-3 py-2 rounded-md transition-colors" on:click={logout}>
-        Sair
+      <button class="block px-3 py-2 rounded-md transition-colors hover:bg-btn-primary hover:text-white" on:click={logout}>
+        <FontAwesomeIcon icon={faDoorOpen} class="w-4 h-4" /> Sair
       </button>
     </li>
   </ul>
