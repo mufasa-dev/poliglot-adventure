dd0a2e83449ebd333b4be624e351ed2742006c7e
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-10-01 22:04:25 -0300

    feat: install astro / vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import vercel from "@astrojs/vercel";
 import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+  output: "server", 
+  adapter: vercel({}),
 });
